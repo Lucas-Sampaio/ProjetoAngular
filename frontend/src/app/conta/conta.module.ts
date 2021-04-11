9f45931b6abdfeab50a3c07c1079f9fd8523d842
Lucas Sampaio <lucasssouza29@hotmail.com>
2021-04-11 01:19:00 -0300

    Adicionado form de usuario

--- frontend/src/app/conta/conta.module.ts
+++ frontend/src/app/conta/conta.module.ts
@@ -1,21 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
+
 import { ContaRoutingModule } from './conta.route';
-import { RouterModule } from '@angular/router';
-
-
-
+import { ContaAppComponent } from './conta.app.component';
+import { ContaService } from './services/conta.service';
+import { CustomFormsModule } from 'ng2-validation';
 @NgModule({
   declarations: [
+    ContaAppComponent,
     CadastroComponent,
     LoginComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
-    ContaRoutingModule
+    ContaRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    CustomFormsModule
+  ],
+  providers: [
+    ContaService
   ]
 })
 export class ContaModule { }
