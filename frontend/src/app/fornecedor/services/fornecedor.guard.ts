cd6f08895b6d3536743d0c68e494f913b6fe6ccc
Lucas Sampaio <lucasssouza29@hotmail.com>
2021-04-28 11:59:24 -0300

    refatorando guard

--- frontend/src/app/fornecedor/services/fornecedor.guard.ts
+++ frontend/src/app/fornecedor/services/fornecedor.guard.ts
@@ -1,49 +1,13 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
-import { LocalStorageUtils } from "src/app/utils/localstorage";
+import { ActivatedRouteSnapshot, CanActivate, Router } from "@angular/router";
+import { BaseGuard } from "src/app/services/base.guard";
 
 @Injectable()
-export class FornecedorGuard implements CanActivate {
+export class FornecedorGuard extends BaseGuard implements CanActivate {
 
-  localSorageUtils = new LocalStorageUtils();
+  constructor(protected router: Router) { super(router) }
 
-  constructor(private router: Router) { }
-
-  canActivate(routeAc: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-
-    if (!this.localSorageUtils.obterTokenUsuario()) {
-      this.router.navigate(["/conta/login"]);
-    }
-
-    let user = this.localSorageUtils.obterUsuario();
-    let claim: any = routeAc.data[0];
-
-    if (claim !== undefined) {
-      let claim = routeAc.data[0]['claim'];
-
-      if (claim) {
-        if (!user.claims) {
-          this.navegarAcessoNegado();
-        }
-
-        let userClaims = user.claims.find(x => x.type === claim.nome);
-
-        if (!userClaims) {
-          this.navegarAcessoNegado();
-        }
-
-        let valoresClaim = userClaims.value as string;
-
-        if (!valoresClaim.includes(claim.valor)) {
-          this.navegarAcessoNegado();
-        }
-      }
-    }
-
-    return true;
-  }
-
-  navegarAcessoNegado() {
-    this.router.navigate(['/acesso-negado']);
+  canActivate(routeAc: ActivatedRouteSnapshot) {
+    return super.validarClaims(routeAc)
   }
 }
