41dcee9130622bf6a9b7712da9c56a7a36e1499d
Lucas Sampaio <lucasssouza29@hotmail.com>
2021-04-27 22:15:42 -0300

    adicionado edicao e detalhes

---
+++ frontend/src/app/produto/detalhes/detalhes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Produto } from '../models/produto';
+
+@Component({
+  selector: 'app-detalhes',
+  templateUrl: './detalhes.component.html',
+})
+export class DetalhesComponent implements OnInit {
+
+  produto: Produto;
+
+  constructor(private route: ActivatedRoute) {
+    this.produto = this.route.snapshot.data['produto'];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
